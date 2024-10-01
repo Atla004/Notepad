9c4beb06e50344049015a5120c744ab38795e9db
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-01 01:24:19 -0400

    feat: Add FABNewNote component

--- app/home.tsx
+++ app/home.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import { Avatar, Card, IconButton, Text } from "react-native-paper";
 import { Searchbar } from "react-native-paper";
+import FABNewNote from "@/components/FABNewNote";
 
 const data = [
   {
@@ -41,121 +42,16 @@ export default function Home() {
         value={searchQuery}
       />
       <FlatList data={data} renderItem={() => <Note />} />
+      <FABNewNote />
     </>
   );
 }
-
-/* 
-import * as React from 'react';
-import { Avatar, Card, IconButton } from 'react-native-paper';
-
-const MyComponent = () => (
-  <Card.Title
-    title="Card Title"
-    subtitle="Card Subtitle"
-    left={(props) => <Avatar.Icon {...props} icon="folder" />}
-    right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
-  />
-);
-
-export default MyComponent; 
-*/
-
-/* 
-import * as React from 'react';
-import { Searchbar } from 'react-native-paper';
-
-const MyComponent = () => {
-  const [searchQuery, setSearchQuery] = React.useState('');
-
-  return (
-    <Searchbar
-      placeholder="Search"
-      onChangeText={setSearchQuery}
-      value={searchQuery}
-    />
-  );
-};
-
-export default MyComponent; 
-*/
-
-/* 
-import React from 'react';
-import {
-  StyleProp,
-  ViewStyle,
-  Animated,
-  StyleSheet,
-  Platform,
-  ScrollView,
-  Text,
-  SafeAreaView,
-  I18nManager,
-} from 'react-native';
-import { AnimatedFAB } from 'react-native-paper';
-
-const MyComponent = ({
-  animatedValue,
-  visible,
-  extended,
-  label,
-  animateFrom,
-  style,
-  iconMode,
-}) => {
-  const [isExtended, setIsExtended] = React.useState(true);
-
-  const isIOS = Platform.OS === 'ios';
-
-  const onScroll = ({ nativeEvent }) => {
-    const currentScrollPosition =
-      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
-
-    setIsExtended(currentScrollPosition <= 0);
-  };
-
-  const fabStyle = { [animateFrom]: 16 };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView onScroll={onScroll}>
-        {[...new Array(100).keys()].map((_, i) => (
-          <Text>{i}</Text>
-        ))}
-      </ScrollView>
-      <AnimatedFAB
-        icon={'plus'}
-        label={'Label'}
-        extended={isExtended}
-        onPress={() => console.log('Pressed')}
-        visible={visible}
-        animateFrom={'right'}
-        iconMode={'static'}
-        style={[styles.fabStyle, style, fabStyle]}
-      />
-    </SafeAreaView>
-  );
-};
-
-export default MyComponent;
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-  },
-  fabStyle: {
-    bottom: 16,
-    right: 16,
-    position: 'absolute',
-  },
-}); 
-*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    flexGrow: 1,
   },
   item: {
     padding: 20,
@@ -165,4 +61,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
-});
+  fabStyle: {
+    bottom: 16,
+    right: 16,
+    position: 'absolute',
+  },
+}); 
