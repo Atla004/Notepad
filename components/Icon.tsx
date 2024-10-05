376d8c4f9b912578745d9b9f072ed678aec85199
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-05 00:57:39 -0400

    feat: Add Icon component
    Add Icon component with various icons like star, sun, more, trash, folder, and box.

---
+++ components/Icon.tsx
@@ -0,0 +1,31 @@
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+
+
+export const FavoritesIcon=() =>{
+  return <Entypo name="star-outlined" size={24} color="black" />
+}
+
+export const SunIcon=() =>{
+  return <Feather name="sun" size={24} color="black" />
+}
+
+export const MoreIcon=() =>{
+  return <Feather name="more-vertical" size={24} color="black" />
+}
+
+export const TrashIcon=() =>{
+  return <FontAwesome5 name="trash" size={24} color="black" />
+}
+
+export const FolderIcon=() =>{
+  return <FontAwesome6 name="folder" size={24} color="black" />
+}
+
+export const BoxIcon=() =>{
+  return <FontAwesome6 name="box" size={24} color="black" />
+}
