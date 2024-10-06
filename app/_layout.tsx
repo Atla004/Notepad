0d1ad0404258c284394ff4b0eddc97cca559f508
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-06 04:26:46 -0400

    refactor: Update Layout component to use Container component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,16 @@
 import * as React from "react";
-
-import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
 import { Stack } from "expo-router";
+import Container from "@/components/Container";
 
 export default function Layout() {
   return (
-    <PaperProvider>
+    <Container>
         <Stack 
         screenOptions={
           {headerShown: false}
         }
         />
-    </PaperProvider>
+    </Container>
   );
 }
 
