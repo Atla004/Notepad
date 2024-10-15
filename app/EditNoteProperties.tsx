0cb103a93e58f8af209b3dfe5f3ba9f7fc3e082e
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-15 03:17:26 -0400

    feat: add dropdown and improvement in chips styles

--- app/EditNoteProperties.tsx
+++ app/EditNoteProperties.tsx
@@ -1,46 +1,65 @@
-import { Stack, useLocalSearchParams } from "expo-router";
-import { Pressable, StyleSheet, View } from "react-native";
-import {NoteHtml} from "@/components/NoteHtml";
-import { FavoritesIcon,TrashIcon } from "@/components/Icon";
-import { TextInput } from "react-native-paper";
-import { useState } from "react";
-
-import { Chip,Button } from 'react-native-paper';
-import { PlusIcon } from "@/components/Icon";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import {
+  Chip,
+  TextInput,
+  Divider,
+  Dialog,
+  Portal,
+  Text,
+} from "react-native-paper";
+import { Pressable } from "react-native";
+import { FavoritesIcon, TrashIcon } from "@/components/Icon";
 import CategorySheet from "@/components/CategorySheet";
-
-
-
+import DropdownPriority from "@/components/DropdownPriority";
+import { Stack } from "expo-router";
 
 const EditNoteProperties = () => {
   const [noteName, setNoteName] = useState("");
+  const [visible, setVisible] = useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
+  const data = [
+    { id: 1, title: "Category 1" },
+    { id: 2, title: "Category 2" },
+    { id: 3, title: "Category 3" },
+    { id: 4, title: "Category 4ffff" },
+    { id: 5, title: "Category 5" },
+    // Agrega más elementos según sea necesario
+  ];
 
   return (
-
     <View>
       <Stack.Screen
-      options={{
-        title: `Edit Note Properties`,
-        headerShown: true,
-        headerRight: () => {
-          return (
-            <>
-              <Pressable
-                onPress={() => console.log('Bookmark')}
-              >
-                <FavoritesIcon/>
-              </Pressable>
-              <Pressable
-                onPress={() => console.log('Bookmark')}
-              >
-                <TrashIcon/>
-              </Pressable>
+        options={{
+          title: `Edit Note Properties`,
+          headerShown: true,
+          headerRight: () => {
+            return (
+              <>
+                <Pressable onPress={() => console.log("Bookmark")}>
+                  <FavoritesIcon />
+                </Pressable>
+                <Pressable onPress={showDialog}>
+                  <TrashIcon />
+                </Pressable>
 
-            </>
-          );
-        }
-
-      }}
+                <Portal>
+                  <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Icon icon="alert" />
+                    <Dialog.Title style={styles.title}>
+                      This is a title
+                    </Dialog.Title>
+                    <Dialog.Content>
+                      <Text variant="bodyMedium">This is simple dialog</Text>
+                    </Dialog.Content>
+                  </Dialog>
+                </Portal>
+              </>
+            );
+          },
+        }}
       />
 
       <TextInput
@@ -48,32 +67,68 @@ const EditNoteProperties = () => {
         label="name"
         value={noteName}
         mode="outlined"
-        onChangeText={(text) => setNoteName(text)}      
+        onChangeText={(text) => setNoteName(text)}
       />
-      <Chip icon="close" onPress={() => console.log('Pressed')}>Example Chip</Chip>
-      <Chip icon="close" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Divider />
+      <Text variant="bodySmall">Categories</Text>
+      <View style={styles.chipContainer}>
+        {data.map((item) => (
+          <Chip
+            key={item.id}
+            icon="close"
+            onPress={() => console.log("close")}
+            style={styles.chips}
+          >
+            <Text
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              variant="bodySmall"
+              style={styles.chipText}
+            >
+              {item.title}
+            </Text>
+          </Chip>
+        ))}
+      </View>
 
-
-
-      <CategorySheet/>            
-
+      <CategorySheet />
+      <Divider />
+      <Text variant="bodySmall">Priority</Text>
+      <DropdownPriority />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   input: {
-    maxWidth: 200, // Set the maximum width to 300
-    width: "100%", // Ensure the input takes up the full width of its container
+    maxWidth: 200,
+    width: "100%",
   },
   card: {
     width: 250,
   },
   container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+  },
+  chipContainer: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  chips: {
+    margin: 4,
+    width: 100,
+  },
+  chipText: {
+    width: "100%",
   },
 });
 
