2750d82e24528d9840cf3d1bffceb9cf91e406c1
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-16 22:31:32 -0400

    refactor: remove Footer component from Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { Stack } from "expo-router";
 import Container from "@/components/Container";
-import Footer from "@/components/FOoter";
 
 export default function Layout() {
   return (
     <Container>
       <Stack screenOptions={{ headerShown: false }} />
-      <Footer />
     </Container>
   );
 }
