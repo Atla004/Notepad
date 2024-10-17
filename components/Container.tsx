659fc6dbef39e4dc1a219461997455df12e249a5
Atla004 <andrese.g.v.13579@gmail.com>
2024-10-16 22:56:21 -0400

    refactor: improve styles in Profile component

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
+  shadow,
   Surface,
 } from "react-native-paper";
 
@@ -13,6 +14,7 @@ const theme = {
     surface: "#EBEBD3", // Beige
     primaryContainer: "#F7F7ED", //white
     tertiary: "#532302", //marron
+    shadow: "#3C3C3B", // gris
   },
   roundness: 8, // Añadir más propiedades si es necesario
   // Puedes añadir más propiedades según sea necesario
