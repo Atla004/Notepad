0433b4c10d0bcd43bc901578ae4260d95d2142d6
Rafael Mata <matarafael2004@gmail.com>
2024-10-17 21:31:34 -0400

    feat: added functionality to changeUserEmail and deleteUser methods

--- services/user.ts
+++ services/user.ts
@@ -1,7 +1,41 @@
-export const changeUserEmail = async (username: string, newEmail: string) => {
+import { wrappedFetch } from "./fetch";
+import { storeData, storeExpiringData } from "./localstorage";
+import { FetchError } from "./utils";
 
+export const changeUserEmail = async (userData: {username: string, newEmail: string}) => {
+    try {
+        const response = await wrappedFetch({
+            route: `/user/${userData.username}/change-email`,
+            method: "PUT",
+            body: userData,
+        });
+        if (response.status !== 200) {
+            const errors = await response.json();
+            throw new FetchError(errors.error);
+        }
+
+        return;
+    } 
+    catch (error) {
+        throw new Error(`Failed to change email`);
+    }
 }
 
-export const deleteUser = async (username: string) => {
-
+export const deleteUser = async (userData: {username: string}) => {
+    try {
+        const response = await wrappedFetch({
+            route: `/user/${userData.username}`,
+            method: "DELETE",
+            body: userData,
+        });
+        if (response.status !== 200) {
+            const errors = await response.json();
+            throw new FetchError(errors.error);
+        }
+        return;
+        
+    } 
+    catch (error) {
+        throw new Error(`Failed to delete user`);
+    } 
 };
